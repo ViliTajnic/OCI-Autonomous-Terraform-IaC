d144f3577dc9a8f61cf4344457c42e2cb797b6c7
Vili Tajnic <vili@lokalno.com>
2025-07-03 10:55:49 +0200

    major update of logic

--- compute.tf
+++ compute.tf
@@ -1,24 +1,11 @@
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.compartment_id
-}
-
-data "oci_core_images" "latest" {
-  compartment_id = var.compartment_id
-  operating_system = "Oracle Linux"
-  operating_system_version = "8"
-  shape = var.use_free_tier ? "VM.Standard.A1.Flex" : var.compute_shape
-  sort_by = "TIMECREATED"
-  sort_order = "DESC"
-}
-
 resource "oci_core_instance" "vm" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_id
-  shape               = var.use_free_tier ? "VM.Standard.A1.Flex" : var.compute_shape
+  shape               = local.compute_shape
 
   shape_config {
-    ocpus         = var.use_free_tier ? 1 : var.compute_ocpus
-    memory_in_gbs = var.use_free_tier ? 1 : var.compute_memory_in_gbs
+    ocpus         = local.compute_ocpus
+    memory_in_gbs = local.compute_memory_in_gbs
   }
 
   source_details {
@@ -28,5 +15,6 @@ resource "oci_core_instance" "vm" {
 
   metadata = {
     ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
+    user_data           = filebase64("${path.module}/cloud-init.yaml")
   }
 }
--- main.tf
+++ main.tf
@@ -21,23 +21,3 @@ resource "oci_database_autonomous_database" "adb" {
   is_free_tier               = var.use_free_tier
   is_auto_scaling_enabled    = false
 }
-
-resource "oci_core_instance" "vm" {
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-  compartment_id      = var.compartment_id
-  shape               = local.compute_shape
-
-  shape_config {
-    ocpus         = local.compute_ocpus
-    memory_in_gbs = local.compute_memory_in_gbs
-  }
-
-  source_details {
-    source_type = "image"
-    source_id   = data.oci_core_images.latest.images[0].id
-  }
-
-  metadata = {
-    ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
-  }
-}
